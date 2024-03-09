28bb835ad6990b6b3b578be2a56fc90d79e5a5a0
peifish1124 <sophiachen1124@gmail.com>
2024-03-09 15:28:15 +0800

    fix: add dotenv to config

--- src/Configs/linebotConfig.ts
+++ src/Configs/linebotConfig.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 const linebotConfig = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
   channelSecret: process.env.CHANNEL_SECRET || 'secret',
